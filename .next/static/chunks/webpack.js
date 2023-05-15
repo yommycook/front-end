777edd499c3a725222e495af5fb7e00b2ec3cc06
Jin <2414487@gmail.com>
2023-05-15 18:30:22 +0900

    recipe form

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "18edd2000ddb44cd"; }
+/******/ 		__webpack_require__.h = function() { return "5fc4e240b7c3bb9a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/recipe-register.js
+++ pages/recipe-register.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
-import { PlusOutlined } from '@ant-design/icons';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import { Button, Form, Input, InputNumber, Select, Typography, Divider, Space, Upload } from 'antd';
 
 const { Title } = Typography;
@@ -98,74 +98,6 @@ const RecipeRegister = () => {
                         </div>
                         <Divider></Divider>
                         <Title level={5}>재료 선택</Title>
-                        <div>
-                            <Form.Item
-                                style={{
-                                    display: 'inline-block',
-                                    marginRight: '15px',
-                                }}
-                                label='재료'
-                            >
-                                <Input placeholder='예) 쇠고기' />
-                            </Form.Item>
-                            <Form.Item
-                                label='정량'
-                                style={{
-                                    display: 'inline-block',
-                                    marginRight: '15px',
-                                }}
-                            >
-                                <InputNumber placeholder='예) 300g'></InputNumber>
-                            </Form.Item>
-                        </div>
-                        <Space wrap>
-                            <Button type='primary'>재료 추가</Button>
-                        </Space>
-                        <Divider></Divider>
-                        <Title level={5}>요리 순서</Title>
-                        <div>
-                            <Form.Item
-                                style={{
-                                    display: 'inline-block',
-                                }}
-                                label='1 단계'
-                            >
-                                <TextArea
-                                    rows={4}
-                                    placeholder='쇠고기는 기름을 떼어 적당한 크기로 썰어 주세요.'
-                                />
-                            </Form.Item>
-                            <Form.Item
-                                style={{
-                                    display: 'inline-block',
-                                    marginLeft: '20px',
-                                }}
-                                valuePropName='fileList'
-                                getValueFromEvent={normFile}
-                            >
-                                <Upload
-                                    action='/upload.do'
-                                    listType='picture-card'
-                                >
-                                    <div>
-                                        <PlusOutlined />
-                                        <div
-                                            style={{
-                                                marginTop: 8,
-                                            }}
-                                        >
-                                            사진 선택
-                                        </div>
-                                    </div>
-                                </Upload>
-                            </Form.Item>
-                        </div>
-                        <Space wrap>
-                            <Button type='primary'>단계 추가</Button>
-                        </Space>
-                        <Divider></Divider>
-
-                        <Title level={5}>재료 선택</Title>
                         <Form.List name='list_ingredients'>
                             {(fields, { add, remove }) => (
                                 <>
@@ -175,6 +107,7 @@ const RecipeRegister = () => {
                                             align='baseline'
                                         >
                                             <Form.Item
+                                                {...field}
                                                 style={{
                                                     display: 'inline-block',
                                                     marginRight: '15px',
@@ -184,6 +117,7 @@ const RecipeRegister = () => {
                                                 <Input placeholder='예) 쇠고기' />
                                             </Form.Item>
                                             <Form.Item
+                                                {...field}
                                                 label='정량'
                                                 style={{
                                                     display: 'inline-block',
@@ -210,6 +144,79 @@ const RecipeRegister = () => {
                                 </>
                             )}
                         </Form.List>
+                        <Divider></Divider>
+                        <Title level={5}>요리 순서</Title>
+                        <Form.List name='list_ingredients'>
+                            {(fields, { add, remove }) => (
+                                <>
+                                    {fields.map((field) => (
+                                        <Space
+                                            key={field.key}
+                                            align='baseline'
+                                        >
+                                            <Form.Item
+                                                style={{
+                                                    display: 'inline-block',
+                                                }}
+                                                label='1 단계'
+                                            >
+                                                <TextArea
+                                                    rows={4}
+                                                    placeholder='쇠고기는 기름을 떼어 적당한 크기로 썰어 주세요.'
+                                                />
+                                            </Form.Item>
+                                            <Form.Item
+                                                style={{
+                                                    display: 'inline-block',
+                                                    marginLeft: '20px',
+                                                }}
+                                                valuePropName='fileList'
+                                                getValueFromEvent={normFile}
+                                            >
+                                                <Upload
+                                                    action='/upload.do'
+                                                    listType='picture-card'
+                                                >
+                                                    <div>
+                                                        <PlusOutlined />
+                                                        <div
+                                                            style={{
+                                                                marginTop: 8,
+                                                            }}
+                                                        >
+                                                            사진 선택
+                                                        </div>
+                                                    </div>
+                                                </Upload>
+                                            </Form.Item>
+                                            <MinusCircleOutlined
+                                                onClick={() => remove(field.name)}
+                                            />
+                                        </Space>
+                                    ))}
+                                    <Form.Item>
+                                        <Button
+                                            type='dashed'
+                                            onClick={() => add()}
+                                            block
+                                            icon={<PlusOutlined />}
+                                        >
+                                            Add sights
+                                        </Button>
+                                    </Form.Item>
+                                </>
+                            )}
+                        </Form.List>
+
+                        <Divider></Divider>
+                        <Form.Item>
+                            <Button
+                                type='primary'
+                                htmlType='submit'
+                            >
+                                레시피 등록
+                            </Button>
+                        </Form.Item>
                     </Form>
                 </div>
             </AppLayout>
